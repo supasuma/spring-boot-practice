9d657d3547f8f86c4ccce97e03b52b7ec188182a
supasuma <supasuma@gmail.com>
2018-07-31 17:42:04 +0100

    add another endpoint & serivce method

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,5 +1,7 @@
 package io.javabrains.springbootstarter.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +11,19 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic("java", "Core Javak", "Core Java Description"),
-                new Topic("javascript", "Core Javascript", "Core Javascript Description")
-        );
+        return topicService.getAllTopics();
+    }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
     }
 
 }
+
+UP TO HERE https://www.youtube.com/watch?v=AI2oBJkPK3c&list=PLmbC-xnvykcghOSOJ1ZF6ja3aOgZAgaMO&index=19
---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,25 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+
+            new Topic("spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("java", "Core Javak", "Core Java Description"),
+            new Topic("javascript", "Core Javascript", "Core Javascript Description")
+    );
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+}
